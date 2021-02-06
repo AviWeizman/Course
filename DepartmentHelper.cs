26f29581c29e186fea428015a1b00de79adf9f5a
AviWeizman <74554661+AviWeizman@users.noreply.github.com>
2021-02-06 22:35:58 +0200

    Create DepartmentHelper.cs

---
+++ DepartmentHelper.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.net_final_project.Models
+{
+    public class DepartmentHelper
+    {
+        public string DepName { get; set; }
+        public int ID { get; set; }
+        public int Manager { get; set; }
+        public string EmpId { get; set; }
+        public string EmpName { get; set; }
+        
+    }
+}
